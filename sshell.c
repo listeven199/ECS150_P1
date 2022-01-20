f8a734ddc5a6d8f8b6978ba3c868773dbe354983
listeven199 <34220743+listeven199@users.noreply.github.com>
2022-01-20 10:53:25 -0800

    Add files via upload

---
+++ sshell.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define CMDLINE_MAX 512
+
+int main(void)
+{
+        char cmd[CMDLINE_MAX];
+        while (1) {
+                char *nl;
+
+                /* Print prompt */
+                printf("sshell$ ");
+                fflush(stdout);
+
+                /* Get command line */
+                fgets(cmd, CMDLINE_MAX, stdin);
+                
+
+                /* Print command line if stdin is not provided by terminal */
+                if (!isatty(STDIN_FILENO)) {
+                        printf("%s", cmd);
+                        fflush(stdout);
+                }
+
+                /* Remove trailing newline from command line */
+                nl = strchr(cmd, '\n');
+                if (nl)
+                        *nl = '\0';
+                
+                char command[CMDLINE_MAX];
+                strcpy(command, cmd);
+                
+                /* Built-in Commands */
+                if (!strcmp(cmd, "")) {
+                        continue;
+                }
+                if (!strcmp(cmd, "exit")) {
+                        fprintf(stderr, "Bye...\n");
+                        break;
+                }
+                if (!strcmp(cmd, "pwd")) {
+                        char* buf = getcwd(NULL, 0);
+                        int ret = 0;
+                        if (buf == NULL) ret = 1;
+                        printf("%s\n", buf);
+                        printf("+ completed 'pwd' [%d]\n", ret);
+                        continue;
+                }
+
+                /* Regular Commands */
+                char* arguments[16];
+                char* token= strtok(cmd, " ");
+                int argc = 0;
+                while (token != NULL) {
+                        arguments[argc++] = token;
+                        token = strtok(NULL, " ");
+                }
+                argc++;       
+                char* argList[argc];
+                for (int i = 0; i < argc-1; i++) {
+                        argList[i] = arguments[i];
+                }
+                argList[argc-1] = NULL;
+
+                pid_t pid;
+                pid = fork();
+                if (pid == 0) {
+                        execvp(cmd, argList);
+                        perror("execvp");
+                        exit(1);
+                } else if (pid > 0) {
+                        int status;
+                        waitpid(pid, &status, 0);
+                        printf("+ completed '%s' [%d]\n", command, WEXITSTATUS(status));
+                } else {
+                        perror("fork");
+                        exit(1);
+                }
+
+        }
+
+        return EXIT_SUCCESS;
+}
